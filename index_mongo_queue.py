6a51cf4a89a9a728a768d66530968cb73d9c24d7
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-04-03 17:38:53 +0200

    read queries file and prepare requesting

--- index_mongo_queue.py
+++ index_mongo_queue.py
@@ -3,6 +3,7 @@
 
 import sys, re
 from lru import split_lru_in_stems, clean_lru
+from read_queries import read_queries_file
 from neo4j.v1 import GraphDatabase
 from warnings import filterwarnings
 filterwarnings(action='ignore', category=UserWarning, message="Bolt over TLS is only available")
@@ -18,7 +19,14 @@ filterwarnings(action='ignore', category=UserWarning, message="Bolt over TLS is 
 # - page_error: string
 # - page_crawl_depth: integer
 # - page_sources: enum(crawl,link,both) =>
-
+types = {
+  "s": "Scheme",
+  "h": "Host",
+  "t": "Port",
+  "p": "Path",
+  "q": "Query",
+  "f": "Fragment"
+}
 def lru_to_stemnodes(lru):
     stems = []
     lru = clean_lru(lru)
@@ -26,10 +34,10 @@ def lru_to_stemnodes(lru):
     for typ, val, stem in split_lru_in_stems(lru):
         sublru += stem + "|"
         stems.append({
-            "LRU": sublru,
-            "type": typ,
-            "stem": stem,
-            "page": False
+          "lru": sublru,
+          "type": types[typ],
+          "stem": stem,
+          "page": False
         })
     stems[-1]["page"] = True
     return stems
@@ -38,8 +46,10 @@ def lru_to_stemnodes_bigrams(lru):
     stems = lru_to_stemnodes(lru)
     return zip(stems, stems[1:])
 
-def run_load(session):
-    pass
+def run_load(session, queries):
+    lru = "s:http|h:fr|h:sciences-po|h:medialab|"
+    #session.write_transaction(queries["index"], {lrus: [lru_to_stemnodes(lru)]})
+    #session.write_transaction(queries["index"], {lrus: [lru_to_stemnodes(lru)]})
 
 
 if __name__ == "__main__":
@@ -49,6 +59,7 @@ if __name__ == "__main__":
         sys.stderr.write("ERROR: please create & fill config.py from config.py.example first")
         exit(1)
     neo4jdriver = GraphDatabase.driver("bolt://%s:%s" % (neo4j_host, neo4j_port), auth=(neo4j_user, neo4j_pass))
+    with open("queries/notes.cypher") as f:
+        queries = read_queries_file(f)
     with neo4jdriver.session() as session:
-        run_load(session)
-    print(lru_to_stemnodes_bigrams("s:http|h:fr|h:sciences-po|h:medialab|"))
+        run_load(session, queries)
