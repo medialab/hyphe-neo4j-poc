284aa2c86c5948f67cd1f1405dcc4e93050fbcb0
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-04-04 14:50:58 +0200

    reformat conf

--- index_mongo_queue.py
+++ index_mongo_queue.py
@@ -93,7 +93,7 @@ def load_lrus(session, queries, pages=[]):
     for lru, lrulinks, metas in pages:
         lrus += prepare_lrus(lru, lrulinks, metas)
     a = write_query(session, queries["index"], lrus=lrus)
-    print(a._summary.counters.__dict__)
+    #print(a._summary.counters.__dict__)
 
 def run_WE_creation_rule(session, queries, lastcheck):
     we_prefixes = read_query(session, queries["we_default_creation_rule"], lastcheck=lastcheck)
@@ -115,20 +115,20 @@ def run_WE_creation_rule(session, queries, lastcheck):
 if __name__ == "__main__":
     # Load config
     try:
-        import config as cf
+        from config import neo4j_conf, mongo_conf, lrus_batch_size
     except:
         sys.stderr.write("ERROR: please create & fill config.py from config.py.example first")
         exit(1)
 
     # MongoDB Connection
-    mongoconn = MongoClient(cf.mongo_host, cf.mongo_port)[cf.mongo_base][cf.mongo_coll]
+    mongoconn = MongoClient(mongo_conf["host"], mongo_conf["port"])[mongo_conf["base"]][mongo_conf["coll"]]
 
     # Read Neo4J Queries file
     with open("queries/core.cypher") as f:
         queries = read_queries_file(f)
 
     # Neo4J Connection
-    neo4jdriver = GraphDatabase.driver("bolt://%s:%s" % (cf.neo4j_host, cf.neo4j_port), auth=(cf.neo4j_user, cf.neo4j_pass))
+    neo4jdriver = GraphDatabase.driver("bolt://%s:%s" % (neo4j_conf["host"], neo4j_conf["port"]), auth=(neo4j_conf["user"], neo4j_conf["pass"]))
     with neo4jdriver.session() as session:
         # ResetDB
         if len(sys.argv) > 1:
@@ -145,7 +145,7 @@ if __name__ == "__main__":
             ))
             totalsize += len(page["lrulinks"]) + 1
             batchsize += len(page["lrulinks"]) + 1
-            if batchsize >= cf.page_batch:
+            if batchsize >= lrus_batch_size:
                 load_lrus(session, queries, pages)
                 pages = []
                 batchzize = 0
