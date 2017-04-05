047727030ef9f73a3ae9f65cdb9a25efefcc9752
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-04-05 14:17:13 +0200

    add index on jobs

--- index_mongo_queue.py
+++ index_mongo_queue.py
@@ -178,6 +178,8 @@ if __name__ == "__main__":
     # MongoDB Connection
     mongodb = MongoClient(mongo_conf["host"], mongo_conf["port"])
     mongoconn = mongodb[mongo_conf["base"]][mongo_conf["coll"]]
+    print "Creating index"
+    mongoconn.ensure_index("_job")
 
     # Read Neo4J Queries file
     with open("queries/core.cypher") as f:
