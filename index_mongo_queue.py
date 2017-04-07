073f3601545140f17b7841d429718b45da475abb
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-04-07 10:58:14 +0200

    move dummy data

--- index_mongo_queue.py
+++ index_mongo_queue.py
@@ -11,50 +11,7 @@ from neo4j.v1 import GraphDatabase
 from warnings import filterwarnings
 filterwarnings(action='ignore', category=UserWarning,
                message="Bolt over TLS is only available")
-
-TEST_DATA = {
-  "pages": [(
-    "s:http|h:fr|h:sciences-po|h:medialab|p:people|",
-    [
-      "s:http|h:fr|h:sciences-po|h:medialab|p:projets|",
-      "s:http|h:com|h:twitter|p:medialab_ScPo|",
-      "s:http|h:com|h:twitter|p:paulanomalie|",
-      "s:http|h:fr|h:sciences-po|h:www|p:bibliotheque|",
-      "s:https|h:com|h:twitter|"
-    ],
-    {
-      "encoding": "utf-8",
-      "depth": 0,
-      "error": None,
-      "status": 200,
-      "timestamp": int(time())
-    }
-  )],
-  "links": [
-    ["s:http|h:fr|h:sciences-po|h:medialab|p:people|",
-     "s:http|h:fr|h:sciences-po|h:medialab|p:projets|"],
-    ["s:http|h:fr|h:sciences-po|h:medialab|p:people|",
-     "s:http|h:com|h:twitter|p:medialab_ScPo|"],
-    ["s:http|h:fr|h:sciences-po|h:www|p:bibliotheque|",
-     "s:http|h:com|h:twitter|"],
-    ["s:http|h:fr|h:sciences-po|h:medialab|p:people|",
-     "s:http|h:com|h:twitter|p:paulanomalie|"],
-    ["s:http|h:com|h:twitter|p:paulanomalie|",
-     "s:https|h:com|h:twitter|"],
-    ["s:http|h:com|h:twitter|p:paulanomalie|",
-     "s:https|h:com|h:twitter|"]
-  ],
-  "WECRs": [
-    {'prefix': '', 'pattern': 'domain'},
-    {'prefix': 's:http|h:com|h:twitter|', 'pattern': 'path-1'},
-    {'prefix': 's:http|h:com|h:facebook|', 'pattern': 'path-1'},
-    {'prefix': 's:http|h:com|h:linkedin|', 'pattern': 'path-2'}
-  ],
-  "manual_webentities": [
-    "s:http|h:fr|h:sciences-po|h:medialab|",
-    "s:http|h:fr|h:sciences-po|h:medialab|h:tools|"
-  ]
-}
+from dummy_data import TEST_DATA
 
 def write_query(session, query, **kwargs):
     return session.write_transaction(lambda tx: tx.run(query, **kwargs))
